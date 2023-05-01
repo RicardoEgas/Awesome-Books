3904a6000e45eebb9a81db689d124ddc63fcbbbd
RicardoEgas <rj_egas@hotmail.com>
2023-05-01 21:45:04 +0200

    Created ShowBooks function

--- script.js
+++ script.js
@@ -8,23 +8,25 @@ class Books {
 
 const buttonAdd = document.querySelector('.button-add');
 const displayBooks = document.getElementById('books-list');
+const removeBtn = document.querySelector('.remove-btn');
 
 
 function getBooks(){
 
-    const author = document.getElementById('author').value;
-    const title = document.getElementById('title').value;
-    const newBook = new Books(title, author);
-    
+
     showBooks();
    }
 
 function showBooks () {
+    const author = document.getElementById('author').value;
+    const title = document.getElementById('title').value;
+    const newBook = new Books(title, author);
     
-    displayBooks.insertAdjacentHTML("afterbegin", `<p>${title}</p> <p>${author}</p> <button class="remove-btn">remove</button>`)
+    displayBooks.insertAdjacentHTML("afterbegin", `<p>${newBook.title}</p> <p>${newBook.author}</p> <button class="remove-btn">remove</button>`)
 }
 
-buttonAdd.addEventListener('click',getBooks);
+buttonAdd.addEventListener('click', getBooks);
+
 
 
 
