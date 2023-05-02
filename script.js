7e91a8fad9d38b7f8f1d0dce42b2a13d5e4849ef
RicardoEgas <rj_egas@hotmail.com>
2023-05-02 17:54:38 +0200

    Implemented the remove Storage function

--- script.js
+++ script.js
@@ -5,33 +5,15 @@ class Books {
     }
 }
 
-
 const buttonAdd = document.querySelector('.button-add');
 const displayBooks = document.getElementById('books-list');
 const removeBtn = document.querySelector('.remove-btn');
 const bookShelf = [];
 
-// Store Class: Handles Storage
+let books = storeGet();
+books.forEach((book) => displayBooks.insertAdjacentHTML("afterbegin", `<div><p>${book.title}</p> <p>${book.author}</p> <button class="remove-btn">remove</button></div>`));
 
-   function storeGet() {
-      let books;
-      if(localStorage.getItem('books') === null) {
-        books = [];
-      } else {
-        books = JSON.parse(localStorage.getItem('books'));
-      }
-  
-      return books;
-    }
-    function storeAdd(newBook) {
-        const books = storeGet();
-        books.push(newBook);
-        localStorage.setItem('books', JSON.stringify(books));
-      }
-  
-    let books = storeGet();
-    books.forEach((book) => displayBooks.insertAdjacentHTML("afterbegin", `<div><p>${book.title}</p> <p>${book.author}</p> <button class="remove-btn">remove</button></div>`));
-
+    
 function showBooks () {
     const author = document.getElementById('author').value;
     const title = document.getElementById('title').value;
@@ -44,15 +26,42 @@ function showBooks () {
 
 buttonAdd.addEventListener('click', showBooks);
 
-
-
 document.addEventListener('click', (e) => {
   const target = e.target.closest('.remove-btn');
+  const title = target.previousElementSibling.previousElementSibling.textContent;
+  
 
   if (target) {
     target.parentElement.remove();
   }
 
+  console.log(title);
+  storeDelete(title);
+
 })
 
+// Storage functions
 
+function storeGet() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+  function storeAdd(newBook) {
+      const books = storeGet();
+      books.push(newBook);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+    function storeDelete(title) {
+      const books = storeGet();
+      const index = books.findIndex((book) => book.title === title);
+      if (index !== -1) {
+        books.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(books));
+      }
+    }
